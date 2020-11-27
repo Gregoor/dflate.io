96fb25064a207a81592772b0509b228c758b1c86
Gregor Weber <gregorwbr@gmail.com>
2020-11-27 14:45:20 +0000

    newest first page ordering

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,15 +19,18 @@ export default ({ pages }: StaticProps<typeof getStaticProps>) => (
     <Head>
       <title>dflate.io</title>
     </Head>
-    {pages.map(({ slug, title, date }) => (
-      <div key={slug}>
-        <Link href={`/${slug}`}>
-          <a>
-            <h2>{title}</h2>
-          </a>
-        </Link>
-        <time>{new Date(date).toISOString().split("T")[0]}</time>
-      </div>
-    ))}
+    {pages
+      .map((page) => ({ ...page, date: new Date(page.date) }))
+      .sort((p1, p2) => (p1.date > p2.date ? -1 : 1))
+      .map(({ slug, title, date }) => (
+        <div key={slug}>
+          <Link href={`/${slug}`}>
+            <a>
+              <h2>{title}</h2>
+            </a>
+          </Link>
+          <time>{date.toISOString().split("T")[0]}</time>
+        </div>
+      ))}
   </Layout>
 );
